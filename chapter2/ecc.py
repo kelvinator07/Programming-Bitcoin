5173120249563e7a38cf05568683747e6bb83e73
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-08-26 05:20:46 +0100

    Updated Chap1 And Chap2

--- chapter2/ecc.py
+++ chapter2/ecc.py
@@ -33,8 +33,7 @@ class Point:
 
     def __add__(self, other):  # <2>
         if self.a != other.a or self.b != other.b:
-            raise TypeError('Points {}, {} are not on the same curve'.format
-            (self, other))
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
 
         if self.x is None:  # <3>
             return other
@@ -45,6 +44,7 @@ class Point:
         # Result is point at infinity
         if self.x == other.x and self.y != other.y:
             return self.__class__(None, None, self.a, self.b)
+
         # Case 2: self.x ≠ other.x
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
@@ -55,19 +55,35 @@ class Point:
             x = s ** 2 - self.x - other.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
+
+        # Case 4: if we are tangent to the vertical line,
+        # we return the point at infinity
+        # note instead of figuring out what 0 is for each type
+        # we just use 0 * self.x
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
         # Case 3: self == other
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
-        if self == other and self.y == 0 * self.x:
-            return self.__class__(None, None, self.a, self.b)
-            # tag::answer7[]
         if self == other:
             s = (3 * self.x ** 2 + self.a) / (2 * self.y)
             x = s ** 2 - 2 * self.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self  # <1>
+        result = self.__class__(None, None, self.a, self.b)  # <2>
+        while coef:
+            if coef & 1:  # <3>
+                result += current
+            current += current  # <4>
+            coef >>= 1  # <5>
+        return result
 
 
 class PointTest(TestCase):
