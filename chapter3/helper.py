fcb4feaa9d3d8ce5c4ae4c3eb372494a92bd9149
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-09-02 13:54:57 +0100

    Chapter 5 - Transactions

--- chapter3/helper.py
+++ chapter3/helper.py
@@ -53,6 +53,10 @@ def decode_base58(s):
     return combined[1:-4]
 
 
+"""Remember, when converting from little-endian to big-endian, 
+swap each pair of characters first, then reverse the string."""
+
+
 def little_endian_to_int(b):
     """little_endian_to_int takes byte sequence as a little-endian number.
     Returns an integer"""
@@ -65,6 +69,37 @@ def int_to_little_endian(n, length):
     byte sequence of length"""
     # use n.to_bytes()
     return n.to_bytes(length, 'little')
+
+
+def read_varint(s):
+    """read_varint reads a variable integer from a stream"""
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+
+def encode_varint(i):
+    """encodes an integer as a varint"""
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
 
 
 class HelperTest(TestCase):
